4551105c7e1f1841ca662fa01f79e2f9fa4cefab
Mohamed El-Serafy <elserafy02@gmail.com>
2024-08-03 13:29:48 +0300

    day 3
    add main

--- day 2/ecommerce/src/app/app.component.ts
+++ day 2/ecommerce/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Item } from './models/item.interface';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FilterDropDownComponent } from './components/filter-drop-down/filter-drop-down.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,50 +14,18 @@ import { FooterComponent } from './components/footer/footer.component';
     NavbarComponent,
     FilterDropDownComponent,
     FooterComponent,
+    MainComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'ecommerce';
-  itemsList!: Item[];
-  typesList!: string[];
-  typesChosen!: string[];
   searchValue: string = '';
-  filteredList: Item[] = [];
-  itemSrv = inject(ItemService);
-
-  ngOnInit(): void {
-    this.itemsList = this.itemSrv.getItems();
-    this.typesList = this.itemsList.map((i) => i.type);
-    this.typesList = [...new Set(this.typesList)];
-    this.filteredList = this.itemsList;
-    this.typesChosen = this.typesList;
-  }
-
-  handleTypes(resultTypes: string[]) {
-    console.log(resultTypes.length);
-    if (!resultTypes.length) {
-      this.typesChosen = this.typesList;
-    } else {
-      this.typesChosen = resultTypes;
-      console.log(this.typesChosen);
-    }
-
-    this.getFilteredElements();
-  }
 
   handleSearch(result: string) {
     console.log(result);
     this.searchValue = result;
-    this.getFilteredElements();
-  }
-
-  getFilteredElements(): void {
-    this.filteredList = this.itemsList.filter(
-      (e) =>
-        this.typesChosen.includes(e.type) &&
-        e.name.toLowerCase().includes(this.searchValue.toLowerCase())
-    );
+    console.log('sending', result);
   }
 }
---
+++ day 2/ecommerce/src/app/components/main/main.component.ts
@@ -0,0 +1,59 @@
+import {
+  Component,
+  inject,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { Item } from '../../models/item.interface';
+import { ItemService } from '../../services/item.service';
+import { FilterDropDownComponent } from '../filter-drop-down/filter-drop-down.component';
+import { ItemComponent } from '../item/item.component';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [ItemComponent, FilterDropDownComponent, MainComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css',
+})
+export class MainComponent implements OnInit, OnChanges {
+  itemsList!: Item[];
+  typesList!: string[];
+  typesChosen!: string[];
+  @Input() searchValue!: string;
+  filteredList: Item[] = [];
+  itemSrv = inject(ItemService);
+  ngOnChanges(changes: SimpleChanges): void {
+    this.getFilteredElements();
+  }
+
+  ngOnInit(): void {
+    this.itemsList = this.itemSrv.getItems();
+    this.typesList = this.itemsList.map((i) => i.type);
+    this.typesList = [...new Set(this.typesList)];
+    this.filteredList = this.itemsList;
+    this.typesChosen = this.typesList;
+  }
+
+  handleTypes(resultTypes: string[]) {
+    console.log(resultTypes.length);
+    if (!resultTypes.length) {
+      this.typesChosen = this.typesList;
+    } else {
+      this.typesChosen = resultTypes;
+      console.log(this.typesChosen);
+    }
+
+    this.getFilteredElements();
+  }
+
+  getFilteredElements(): void {
+    this.filteredList = this.itemsList.filter(
+      (e) =>
+        this.typesChosen.includes(e.type) &&
+        e.name.toLowerCase().includes(this.searchValue.toLowerCase())
+    );
+  }
+}
